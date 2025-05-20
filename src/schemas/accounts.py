c1d4518e5cfd455f359fb8ea1e1a84ffee96855b
struhanets <posseydon87@gmail.com>
2025-05-20 17:20:12 +0200

    Add User, UserCreate, UserRead schemas

--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -3,4 +3,15 @@ from pydantic import BaseModel, EmailStr, field_validator
 from database import accounts_validators
 
 
-# Write your code here
+class User(BaseModel):
+    email: EmailStr
+
+
+class UserCreate(User):
+    password: str
+
+
+class UserRead(User):
+    id: int
+
+
