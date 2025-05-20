6b43bf031637644296097a12feee79d9ac71182b
struhanets <posseydon87@gmail.com>
2025-05-20 17:20:39 +0200

    Add register endpoint

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -15,11 +15,37 @@ from database import (
     UserGroupEnum,
     ActivationTokenModel,
     PasswordResetTokenModel,
-    RefreshTokenModel
+    RefreshTokenModel,
 )
 from exceptions import BaseSecurityError
+from schemas.accounts import UserCreate, UserRead
 from security.interfaces import JWTAuthManagerInterface
+from security.passwords import hash_password
+from security.token_manager import JWTAuthManager
 
 router = APIRouter()
 
-# Write your code here
+
+@router.post("/register", response_model=UserRead)
+async def register_user(user: UserCreate, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(UserModel).where(UserModel.email == user.email))
+    if result.scalar_one_or_none():
+        raise HTTPException(status_code=409, detail=f"A user with this email {user.email} already exists.")
+
+    group_result = await db.execute(select(UserGroupModel).where(UserGroupModel.name == UserGroupEnum.USER))
+    user_group = group_result.scalar_one()
+    new_user = UserModel(
+        email=user.email,
+        is_active=True,
+        group=user_group,
+    )
+    new_user.password = user.password
+    db.add(new_user)
+    await db.flush()
+
+    activation_token = ActivationTokenModel(user=new_user.id)
+    db.add(activation_token)
+
+    await db.commit()
+    await db.refresh(new_user)
+    return new_user
