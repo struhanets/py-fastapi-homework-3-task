7a6bf0de2e8d72b38cdbd833a979b849e6ad8bbc
struhanets <posseydon87@gmail.com>
2025-05-22 13:42:17 +0200

    Add required schemas

--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -19,3 +19,33 @@ class ActivationTokenRequest(BaseModel):
     email: EmailStr
     token: str
 
+
+UserRegistrationRequestSchema = UserCreate
+UserRegistrationResponseSchema = UserRead
+UserActivationRequestSchema = ActivationTokenRequest
+MessageResponseSchema = dict
+
+
+class PasswordResetRequestSchema(BaseModel):
+    email: EmailStr
+    token: str
+
+
+class PasswordResetCompleteRequestSchema(UserActivationRequestSchema):
+    password: str
+
+
+class UserLoginResponseSchema(BaseModel):
+    pass
+
+
+class UserLoginRequestSchema(BaseModel):
+    pass
+
+
+class TokenRefreshRequestSchema(BaseModel):
+    pass
+
+
+class TokenRefreshResponseSchema(BaseModel):
+    pass
